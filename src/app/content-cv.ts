3c52e41d8461a740ef222046c9ba3fc4f04cc2ea
Franziska <Franziska.Roder@gmx.de>
2025-01-08 16:38:52 +0100

    cv update

--- src/app/content-cv.ts
+++ src/app/content-cv.ts
@@ -1,82 +1,80 @@
 export const cv = 
 {
-    "personal_data": {
-      "address": "Hegelstraße 10, 90409 Nürnberg",
-      "date_of_birth": "1995-05-02",
-      "nationality": "Deutsch",
-      "mobile": "0152 58478776",
-      "email": "Franziska.Roder@gmx.de",
-      "github": "https://github.com/Pizzakoenigin/capybook"
+  "Persoenliche_Daten": {
+    "Anschrift": "Hegelstraße 10, 90409 Nürnberg",
+    "Geburtsdatum": "02.05.1995",
+    "Nationalität": "Deutsch",
+    "Email": "Franziska.Roder[at]gmx.de",
+    "Github": "https://github.com/Pizzakoenigin"
+  },
+  "Faehigkeiten": [
+    "Typescript",
+    "Angular",
+    "React",
+    "HTML und CSS",
+    "Axure RP",
+    "Figma"
+  ],
+  "Berufe": [
+    {
+      "Startdatum": "2024-05",
+      "Enddatum": "2024-11",
+      "Titel": "Associate Frontend Developer",
+      "Unternehmen": "CurveDesign GmbH"
     },
-    "skills": [
-      "Typescript",
-      "Angular",
-      "React",
-      "HTML und CSS",
-      "Axure RP",
-      "Figma"
-    ],
-    "jobs": [
-      {
-        "start_date": "2024-05",
-        "end_date": "2024-11",
-        "title": "Associate Frontend Developer",
-        "company": "CurveDesign GmbH"
-      },
-      {
-        "start_date": "2021-09",
-        "end_date": "2023-04",
-        "title": "Designerin",
-        "company": "Zahnimarkt Trading GmbH"
-      },
-      {
-        "start_date": "2021-09",
-        "end_date": "2023-04",
-        "title": "Servicemitarbeiterin, TZ",
-        "company": "Cineplex Münster"
-      }
-    ],
-    "continuing_education": [
-      {
-        "date": "2024-03",
-        "course": "React-Bibliothek",
-        "institution": "Alfatraining Bildungszentrum GmbH"
-      },
-      {
-        "start_date": "2024-01",
-        "end_date": "2024-02",
-        "course": "Javascript-Developer",
-        "institution": "Alfatraining Bildungszentrum GmbH"
-      }
-    ],
-    "education": [
-      {
-        "start_date": "2016-09",
-        "end_date": "2021-08",
-        "degree": "Design",
-        "institution": "FH Münster"
-      },
-      {
-        "start_date": "2019-09",
-        "end_date": "2020-02",
-        "degree": "Erasmus Semester: Game and App Development",
-        "institution": "IADE Lissabon"
-      },
-      {
-        "start_date": "2016-03",
-        "end_date": "2016-07",
-        "degree": "Produktgestaltung",
-        "institution": "Hochschule für Gestaltung Schwäbisch Gmünd"
-      }
-    ],
-    "languages": {
-      "english": "Sehr gute Englischkenntnisse (C1)",
-      "portuguese": "Portugiesischkenntnisse (A1)"
+    {
+      "Startdatum": "2021-09",
+      "Enddatum": "2023-04",
+      "Titel": "Designerin",
+      "Unternehmen": "Zahnimarkt Trading GmbH"
     },
-    "schooling": {
-      "start_date": "2006",
-      "end_date": "2014",
-      "school": "Gymnasium Dresden Cotta"
+    {
+      "Startdatum": "2021-09",
+      "Enddatum": "2023-04",
+      "Titel": "Servicemitarbeiterin, TZ",
+      "Unternehmen": "Cineplex Münster"
     }
+  ],
+  "Weiterbildung": [
+    {
+      "Datum": "2024-03",
+      "Kurs": "React-Bibliothek",
+      "Einrichtung": "Alfatraining Bildungszentrum GmbH"
+    },
+    {
+      "Startdatum": "2024-01",
+      "Enddatum": "2024-02",
+      "Kurs": "Javascript-Developer",
+      "Einrichtung": "Alfatraining Bildungszentrum GmbH"
+    }
+  ],
+  "Studium": [
+    {
+      "Startdatum": "2016-09",
+      "Enddatum": "2021-08",
+      "Abschluss": "Design",
+      "Einrichtung": "FH Münster"
+    },
+    {
+      "Startdatum": "2019-09",
+      "Enddatum": "2020-02",
+      "Abschluss": "Erasmus Semester: Game and App Development",
+      "Einrichtung": "IADE Lissabon"
+    },
+    {
+      "Startdatum": "2016-03",
+      "Enddatum": "2016-07",
+      "Abschluss": "Produktgestaltung",
+      "Einrichtung": "Hochschule für Gestaltung Schwäbisch Gmünd"
+    }
+  ],
+  "Sprachen": {
+    "Englisch": "Sehr gute Englischkenntnisse (C1)",
+    "Portugiesisch": "Portugiesischkenntnisse (A1)"
+  },
+  "Schulbildung": {
+    "Startdatum": "2006",
+    "Enddatum": "2014",
+    "Schule": "Gymnasium Dresden Cotta"
   }
-  
+}
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { cv } from '../content-cv';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cv',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.css'
 })
 export class CvComponent {
-
+  cv = cv
 }
