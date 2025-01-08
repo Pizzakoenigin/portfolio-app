c6b1af55c8afc68527f54c15d045c066f39b1789
Franziska <Franziska.Roder@gmx.de>
2025-01-08 14:26:23 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'main', component: MainComponent },
+    {path: 'projekte', component: ProjectsComponent},
+    {path: 'lebenslauf', component: CvComponent},
+    {path: '',redirectTo: '/main', pathMatch: 'full'}
+];
