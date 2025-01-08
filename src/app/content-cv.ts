bdb6eeac12a070866ede09628a74f78a5f6eaaf8
Franziska <Franziska.Roder@gmx.de>
2025-01-08 15:22:36 +0100

    cv json

---
+++ src/app/content-cv.ts
@@ -0,0 +1,82 @@
+export const cv = 
+{
+    "personal_data": {
+      "address": "Hegelstraße 10, 90409 Nürnberg",
+      "date_of_birth": "1995-05-02",
+      "nationality": "Deutsch",
+      "mobile": "0152 58478776",
+      "email": "Franziska.Roder@gmx.de",
+      "github": "https://github.com/Pizzakoenigin/capybook"
+    },
+    "skills": [
+      "Typescript",
+      "Angular",
+      "React",
+      "HTML und CSS",
+      "Axure RP",
+      "Figma"
+    ],
+    "jobs": [
+      {
+        "start_date": "2024-05",
+        "end_date": "2024-11",
+        "title": "Associate Frontend Developer",
+        "company": "CurveDesign GmbH"
+      },
+      {
+        "start_date": "2021-09",
+        "end_date": "2023-04",
+        "title": "Designerin",
+        "company": "Zahnimarkt Trading GmbH"
+      },
+      {
+        "start_date": "2021-09",
+        "end_date": "2023-04",
+        "title": "Servicemitarbeiterin, TZ",
+        "company": "Cineplex Münster"
+      }
+    ],
+    "continuing_education": [
+      {
+        "date": "2024-03",
+        "course": "React-Bibliothek",
+        "institution": "Alfatraining Bildungszentrum GmbH"
+      },
+      {
+        "start_date": "2024-01",
+        "end_date": "2024-02",
+        "course": "Javascript-Developer",
+        "institution": "Alfatraining Bildungszentrum GmbH"
+      }
+    ],
+    "education": [
+      {
+        "start_date": "2016-09",
+        "end_date": "2021-08",
+        "degree": "Design",
+        "institution": "FH Münster"
+      },
+      {
+        "start_date": "2019-09",
+        "end_date": "2020-02",
+        "degree": "Erasmus Semester: Game and App Development",
+        "institution": "IADE Lissabon"
+      },
+      {
+        "start_date": "2016-03",
+        "end_date": "2016-07",
+        "degree": "Produktgestaltung",
+        "institution": "Hochschule für Gestaltung Schwäbisch Gmünd"
+      }
+    ],
+    "languages": {
+      "english": "Sehr gute Englischkenntnisse (C1)",
+      "portuguese": "Portugiesischkenntnisse (A1)"
+    },
+    "schooling": {
+      "start_date": "2006",
+      "end_date": "2014",
+      "school": "Gymnasium Dresden Cotta"
+    }
+  }
+  
