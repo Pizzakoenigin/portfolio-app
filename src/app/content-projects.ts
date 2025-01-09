cd2f2146dc51d4416caeb8e303d035fbfd2091d5
Franziska <Franziska.Roder@gmx.de>
2025-01-09 16:51:53 +0100

    project component

---
+++ src/app/content-projects.ts
@@ -0,0 +1,141 @@
+export const projects =
+    [{
+        name: "Mexican Runner",
+        category: "Games",
+        description: "Mit der browser basierten 2D Game-Engine Construct 3 habe ich meine Bachelorarbeit erstellt. Sie wurde mit 1,0 bewertet.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Snake 2.0",
+        category: "Games",
+        description: "Ein Browser-Spiel das mithilfe des Canvas Elements erstellt wurde. In meiner Version gibt es ein Zoom-in und Zoom-out item.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Taschenrechner",
+        category: "React",
+        description: "Ein Taschenrechner der auch mit Kommazahlen rechnen kann.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Dogs",
+        category: "React",
+        description: "Ein Datenbank über Hunde mit selbst gesammelten Informationen.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Capybook",
+        category: "Angular",
+        description: "Ein soziales Netzwerk für das größte Nagetier der Erde.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Games",
+        category: "Angular",
+        description: "Eine Datenbank über Videospiele. Die Spielinformationen werden über die API von RAWG.io abgerufen. Einige UI-Elemente stammen von der Material Google Datenbank.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "Diese Website",
+        category: "",
+        description: "wurde mit Angular gebaut",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    },
+    {
+        name: "",
+        category: "Vanilla JS",
+        description: "Die Website für mein Metal-Fanzine.",
+        photos: [
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+            {
+                path: "example"
+            },
+
+        ]
+    }
+
+
+
+    ]
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { projects } from '../content-projects';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects = projects
 }
