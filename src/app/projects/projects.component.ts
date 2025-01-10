18e88599dab92020d8532b083f0fb5bfe7d2ae2e
Franziska <Franziska.Roder@gmx.de>
2025-01-10 14:44:25 +0100

    add pictures project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { projects } from '../content-projects';
 import { CommonModule } from '@angular/common';
 
@@ -8,6 +8,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
-  projects = projects
+export class ProjectsComponent implements OnInit{
+  projects = projects;
+
+  ngOnInit(): void {
+
+  }
+  sortedCategories() {
+    return Array.from(new Set (this.projects.map(project => project.category))).sort()
+  }
+
+  getProjectsByCategory(category: string) {
+    return this.projects.filter(project => project.category === category)
+  }
 }
