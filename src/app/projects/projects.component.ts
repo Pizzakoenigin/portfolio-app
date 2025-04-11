ebccfdff5d50ee99443948ef56f83c72bb6e5a74
Franziska <Franziska.Roder@gmx.de>
2025-04-11 15:14:53 +0200

    style

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
 import { projects } from '../content-projects';
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +12,7 @@ export class ProjectsComponent implements OnInit{
   projects = projects;
 
   ngOnInit(): void {
-
+    
   }
   sortedCategories() {
     return Array.from(new Set (this.projects.map(project => project.category))).sort()
@@ -21,4 +21,19 @@ export class ProjectsComponent implements OnInit{
   getProjectsByCategory(category: string) {
     return this.projects.filter(project => project.category === category)
   }
+
+  // openSlideshow(){
+  //   let arrayImages = Array.from(document.querySelectorAll('.bandImg img'));
+  //   let currentImageIndex = 0;
+    
+
+  //   document.querySelector('.slideshow').style.display = "block";
+  //   document.querySelector('.slideshow').style.backgroundImage = `url('${img.src}')`;
+  //   // currentImageIndex = arrayImages.indexOf(img);
+  //   // elements.imgDescText.innerText = `${imgDescArray[currentImageIndex]}`
+  //   document.querySelector('.slideshow').style.backgroundPosition = "center";
+  //   document.querySelector('.slideshow').style.backgroundRepeat = "no-repeat";
+  //   document.querySelector('.slideshow').style.backgroundSize = "contain";
+  //   document.querySelector('.container').style.display = "none";
+  // }
 }
